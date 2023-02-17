f1c11dd40590a3a16c18d6c6dd8a7f65f1b41279
nomaan-ahmad <nomaan.ahmad21@gmail.com>
2023-02-17 12:20:13 +0530

    Initial implementation of linked list. Created "Add" functionality in Linked List

---
+++ src/com/nomaan/dsa/linkedList/LinkedList.java
@@ -0,0 +1,88 @@
+package com.nomaan.dsa.linkedList;
+
+public class LinkedList<X> {
+
+    private int size;
+    private Node<X> head;
+    private Node<X> tail;
+    private static class Node <X>{
+        X val;
+        Node<X> next;
+
+        Node(X _val) {
+            val = _val;
+            next = null;
+        }
+    }
+
+    public LinkedList() {
+        head = null;
+        tail = null;
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public boolean add(X val) {
+        if (!isEmpty())
+            if (head.val.getClass() != val.getClass()) return false;
+
+        Node<X> temp = new Node<>(val);
+
+        if (isEmpty()) head = temp;
+        else tail.next = temp;
+        tail = temp;
+        size++;
+
+        return true;
+    }
+
+    // If given position which out of linkedList bound then element will be added to the last
+    public boolean add(X val, int pos) {
+        if (!isEmpty())
+            if (head.val.getClass() != val.getClass()) return false;
+
+        Node<X> ele = new Node<>(val);
+
+        if (isEmpty()) {
+            head = ele;
+            tail = ele;
+
+            size++;
+            return true;
+        }
+
+        if (pos == 0) {
+            ele.next = head;
+            head = ele;
+            size++;
+
+            return true;
+        }
+
+        {
+            Node<X> temp = head;
+            while (temp != null && pos-- > 0) temp = temp.next;
+
+            if (temp == null) {
+                tail.next = ele;
+                tail = ele;
+                size++;
+
+                return true;
+            }
+
+            ele.next = temp.next;
+            temp.next = ele;
+            size++;
+
+            return true;
+        }
+
+    }
+
+    public int size() {
+        return size;
+    }
+}
